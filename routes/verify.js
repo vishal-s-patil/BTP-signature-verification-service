5015540d5c4928a276b1d6da1b97fc08284e208d
vishal <vishalspolicepatil55@gmail.com>
2023-11-06 02:55:00 +0530

    license update

--- routes/verify.js
+++ routes/verify.js
@@ -31,7 +31,7 @@ if (os.platform() == 'win32') {
     } else if (os.arch() == 'x86') {
         var chilkat = require('@chilkat/ck-node11-linux32');
     } else {
-        var chilkat = require('@chilkat/ck-node11-linux64');
+        var chilkat = require('@chilkat/ck-node18-linux64');
     }
 } else if (os.platform() == 'darwin') {
     var chilkat = require('@chilkat/ck-node11-macosx');
